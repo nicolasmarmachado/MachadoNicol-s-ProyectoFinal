3747d7194f1504c3a3a767d3d34b589e6c4312f5
Nico <nicom514@hotmail.com>
2023-04-14 20:29:00 -0300

    versión enviada para el proyecto final del curso de JS de CH

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-//HACER QUE SE BORREN LAS CARDS DE ARTICULOS Y QUE NO SE SUMEN AL CLICKEAR DE NUEVO.
-//QUE HAGA SCROLL A LAS TARJETAS
+//PONER ID EN JSON Y ATRIBUTO DATA-ID EN LA CARD
 //HACER CARRITO PARA QUE SUME
+//LE DOY A VACIARCARRITO Y CUANDO HAGO CLICK DE NUEVO, SIGUE SUMANDO (VER ULTIMAS FUNCINOES AGREGADAS PORQUE ANTES FUNCIONABA)
 // usar sweet alert para cartel error en clima. 
 
 // --------Card con los rubros disponibles--------
@@ -17,6 +17,7 @@ function cardRubros(){
       </div>
     `
   }
+
 }
 cardRubros()
 
@@ -45,9 +46,7 @@ async function mostrarRubro() {
     jsonRubros=await consultaRubros.json()      
     const negociosFiltrados = jsonRubros.filter(item => item.rubro === rubroSeleccionado);
     const nombresNegocios = [...new Set(negociosFiltrados.map(item => item.negocio))];//Para evitar repetir los negocios
-    console.log(nombresNegocios)
     muestraRubro = document.querySelector(".muestraRubro")
-    console.log(muestraRubro)
     muestraRubro.innerHTML = "";
     for (const negocio of nombresNegocios) {
       muestraRubro.innerHTML += `
@@ -59,22 +58,20 @@ async function mostrarRubro() {
       </div>`;
     }
       const cardsNegocio = document.querySelectorAll('.cardNegocio')
-      console.log(cardsNegocio)
 
       cardsNegocio.forEach((item) => {
         item.addEventListener("click", function () {
           negocioSeleccionado = item.getAttribute('data-negocio')
-          console.log(negocioSeleccionado)
         })
       })
   }catch(error){
-    console.log(error)
+    swal("Disculpe las molestias!", "No se han podido obtener los negocios del rubro seleccionado. Intente nuevamente más tarde");
   }
-  // const posicion= muestraRubro.offsetTop;
-  // window.scrollTo({
-  //   top: posicion,
-  //   behavior: 'smooth'
-  // })
+  const posicion= muestraRubro.offsetTop;
+  window.scrollTo({
+    top: posicion,
+    behavior: 'smooth'
+  })
 }
 
 //--------Función para animaciones del click en las cards--------
@@ -99,6 +96,7 @@ function volverRubros(){
   cardContainer.classList.add('animate__backInRight') 
   muestraRubro.classList.add('animate__animated') 
   muestraRubro.classList.add('animate__backOutLeft') 
+  articulos.innerHTML='';
 }
   
 //--------Muestra de los artículos para la venta--------
@@ -117,7 +115,6 @@ function mostrarArticulos() {
   const arrayArtPrecio = [];
   
   let objeto=[];
-  // articulos.innerHTML='';
 
   articulos = document.querySelector("#articulos");
 
@@ -132,15 +129,17 @@ function mostrarArticulos() {
     };
     arrayArtPrecio.push(objeto);
 
-
     articulos.innerHTML += `
+    <div class="infoCard">
       <div class="card cardArticulo"style="width: 18rem;">
         <img src="assets/img/cart.png" class="card-img-top" alt="...">
         <div class="card-body">
-          <p class="card-textNegocio">${objeto.articulo}</p>
-          <p class="card-textNegocio">${objeto.precio}</p>
+          <p class="card-textNegocio titulo">${objeto.articulo}</p>
+          <p class="card-textNegocio precio">${objeto.precio}</p>
+          <a href="#" class="agregarCarrito">Agregar al Carrito</a> 
         </div>
       </div>
+    </div>
     `;
   }
   
@@ -154,17 +153,140 @@ function mostrarArticulos() {
 //--------Borrar elementos al hacer click en 'Subir'--------
 
 let btnSubir;
-btnSubir=document.querySelector('#btnSubir')
+btnSubir=document.querySelector('.btnSubir')
 
 btnSubir.addEventListener('click', reiniciarSitio)
 
 function reiniciarSitio(){
   articulos.innerHTML='';
   muestraRubro.innerHTML='';
-  cardRubros(); 
+  volverRubros();
 }
-  //--------API clima--------
 
+//--------BOTONES PARA COMPRA--------
+
+//--------CARRITO--------
+let articulosCarrito=[]//no const porque sino no suma los productos iguales
+const listaProductos=document.querySelector('.listaProductos')
+const contenedorCarrito=document.querySelector('#listaCarrito tbody')
+const vaciarCarritoBtn=document.querySelector('#vaciarCarrito')
+const carrito=document.querySelector('#carrito')
+
+listaProductos.addEventListener('click', agregarProducto)
+carrito.addEventListener('click', eliminarProducto)
+vaciarCarritoBtn.addEventListener('click', vaciarCarrito)
+
+let producto;
+
+function eliminarProducto(e){
+  e.preventDefault();
+  if(e.target.classList.contains('borrarProducto')){
+    producto=e.target.parentElement.parentElement;
+    const productoId=producto.querySelector('a').getAttribute('data-id');
+    articulosCarrito=articulosCarrito.filter(producto=>producto.id!==productoId)
+    carritoHTML();
+  }
+}
+
+function agregarProducto(e){
+  e.preventDefault()
+  if(e.target.classList.contains('agregarCarrito')){
+    producto=e.target.parentElement.parentElement
+    leerDatosProducto(producto)
+  }
+}
+
+function leerDatosProducto(producto){
+  const infoProducto={
+    imagen: producto.querySelector('img').src,
+    titulo: producto.querySelector('.titulo').textContent,
+    precio: producto.querySelector('.precio').textContent,
+    id: producto.querySelector('a').getAttribute('data-id'),
+    cantidad: 1
+  } 
+
+  if(articulosCarrito.some(producto=>producto.id===infoProducto.id)){
+      const productos=articulosCarrito.map(producto=>{
+        if(producto.id===infoProducto.id){
+          let cantidad = parseInt(producto.cantidad);
+          cantidad += 1;
+          producto.cantidad=cantidad;
+          return producto
+        }else{
+          return producto
+        }
+      })
+      articulosCarrito=productos.slice()
+  }else{
+  }
+  articulosCarrito.push(infoProducto)  
+  carritoHTML()
+}
+
+function carritoHTML(){
+  vaciarCarrito();
+  articulosCarrito.forEach(producto=>{
+    const row=document.createElement('tr')
+    row.innerHTML = `
+    <td>
+      <img src="${producto.imagen}" width="50" alt="">
+    </td>
+    <td>${producto.titulo}</td>
+    <td>${producto.precio}</td>
+    <td>${producto.cantidad}</td>
+    <td>
+      <a href="#" class="borrarProducto" data-id="${producto.id}">Borrar</a>
+    </td>
+    `;
+    contenedorCarrito.appendChild(row);
+  })
+}
+
+function vaciarCarrito(){
+  while(contenedorCarrito.firstChild){
+    contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+  }
+}
+
+//--------Ir a Comprar carrito--------
+const comprarCarritoBtn = document.querySelector('#comprarCarrito');
+
+comprarCarritoBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.location.href = 'compraCarrito.html';
+});
+
+const resumenCompra=document.querySelector('#resumenCompra')
+
+articulosCarrito.forEach(producto=>{
+  const row=document.createElement('tr')
+  row.innerHTML = `
+  <td>
+    <img src="${producto.imagen}" width="50" alt="">
+  </td>
+  <td>${producto.titulo}</td>
+  <td>${producto.precio}</td>
+  <td>${producto.cantidad}</td>
+  <td>
+    <a href="#" class="borrarProducto" data-id="${producto.id}">Borrar</a>
+  </td>
+  `;
+  resumenCompra.appendChild(row);
+})
+
+//--------Volver a Inicio--------
+
+const volverInicio = document.querySelector('.inicio');
+
+volverInicio.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.location.href = 'index.html';
+});
+
+  //--------API clima--------
+// let ciudad=document.querySelector('.busquedaLocacion').value
+// ciudad=encodeURIComponent(ciudad)
+// console.log(ciudad)
 const key='194becd837b9fcdbb0f4a963b0a5f357';
 const ciudad='Franck';
 
@@ -176,15 +298,13 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}`
   let temp=clima.main.temp
   let humedad=clima.main.humidity
   let tempC= temp -273.15
-  let html=document.querySelector('#clima')
+  let html=document.querySelector('.clima')
   html.innerHTML=`  
-  T: ${tempC.toFixed(0)} °C <br>
-  H: ${humedad} %
+ <b> ${ciudad} </b>
+ <b> T: ${tempC.toFixed(0)} °C </b>
+ <b> H: ${humedad} % </b>
   `
 })
 .catch((err)=>{
-  console.log(err)
+  swal("No se pueden obtener los datos del clima de su ciudad");
 })
-
-
-
